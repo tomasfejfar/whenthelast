838f282a5d83a7c832d7c765f48a5d38cea9d909
Tomáš Fejfar <tomas.fejfar@gmail.com>
2019-02-11 10:36:05 +0100

    Firebase cfg

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+var config = {
+  apiKey: "AIzaSyBBiyl0p2rEfUmbMbUI9rEf8DLh-CS-FIU",
+  authDomain: "when-the-last.firebaseapp.com",
+  databaseURL: "https://when-the-last.firebaseio.com",
+  projectId: "when-the-last",
+  storageBucket: "",
+  messagingSenderId: "47917094003"
+};
+export default firebase.initializeApp(config);
